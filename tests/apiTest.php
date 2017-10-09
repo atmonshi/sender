3f0ff15a6c6c436148cb2497d1cc273a80fee0cc
aTmonshi <atmonshi@uqu.edu.sa>
2017-10-09 04:19:38 +0300

    test commit

--- tests/apiTest.php
+++ tests/apiTest.php
@@ -1,14 +1,4 @@
 <?php
-
-
-/**
- *  Corresponding Class to test getcolors class
- *
- *  For each class in your library, there should be a corresponding Unit-Test for it
- *  Unit-Tests should be as much as possible independent from other test going on.
- *
- * @author yourname
- */
 class apiTest extends PHPUnit_Framework_TestCase
 {
 
